9d9cdc32676366d7f829f3298699a9cf755d6443
Angelz04 <angelosbh2004@gmail.com>
2024-04-05 19:43:55 -0500

    Ajuste Json

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const jsonServer = require('json-server');
 const path = require('path');
+const cors = require('cors'); // Importa el módulo CORS
 
 const app = express();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+app.use(cors()); // Habilita CORS
 app.use('/api', middlewares, router);
+
+// Manejador de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Hubo un error en el servidor');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
